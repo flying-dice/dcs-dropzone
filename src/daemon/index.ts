1f6c40701d17c92ad561b9faa5d6ce49c8a0bb18
🐸 <dice-a6db@outlook.com>
2025-10-31 01:01:12 +0000

    **Refactor daemon build process:** Removed in-line binary installation logic from `index.ts`. Introduced new `scripts/build-daemon.ts` to handle asset bundling and build steps. Updated `package.json` build script accordingly.

--- src/daemon/index.ts
+++ src/daemon/index.ts
@@ -1,22 +1,11 @@
 import { serve } from "bun";
-import { ensureDirSync } from "fs-extra";
 import { app } from "./app.ts";
-import appConfig, { WGET_EXECUTABLE_PATH } from "./app-config.ts";
+import appConfig from "./app-config.ts";
 import { getLogger } from "./logger.ts";
 
 console.info(`🌍 DCS Dropzone Daemon Starting...`);
 
 const logger = getLogger("index");
-
-logger.debug("Installing Binaries");
-ensureDirSync(appConfig.binaries.target_directory);
-
-logger.debug("Downloading wget binary");
-const wget = await Bun.fetch(appConfig.binaries.wget);
-logger.debug("Writing wget binary to target directory");
-await Bun.write(WGET_EXECUTABLE_PATH, wget);
-
-logger.debug("Finished installing binaries");
 
 const server = serve({
 	hostname: appConfig.server.host,
